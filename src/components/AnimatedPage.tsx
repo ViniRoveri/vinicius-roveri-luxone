cc1805256679f301b9a0c01f6152fca065c48300
ViniRoveri <viniciusroverirocha@gmail.com>
2023-03-08 15:21:57 -0300

    Adding page transition animations

---
+++ src/components/AnimatedPage.tsx
@@ -0,0 +1,37 @@
+import {AnimatePresence, motion} from 'framer-motion'
+import { useRouter } from 'next/router'
+
+type Props = {
+   children: JSX.Element
+}
+
+const stylesContainer = `relative`
+
+export default function AnimatedPage(props:Props){
+   const router = useRouter()
+
+   const animations = {
+      initial: {
+         clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
+      },
+      animate: {
+         clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+      },
+      exit: {
+         clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)',
+      }
+   }
+
+   const transition = {
+      duration: 0.35,
+      ease: 'easeOut'
+   }
+
+   return (
+      <AnimatePresence initial={true} mode='wait'>
+         <motion.div className={stylesContainer} variants={animations} initial='initial' animate='animate' exit='exit' key={router.route} transition={transition}>
+            {props.children}
+         </motion.div>
+      </AnimatePresence>
+   )
+}
--- src/components/DefaultGitHubIcon.tsx
+++ src/components/DefaultGitHubIcon.tsx
@@ -1,7 +1,8 @@
 import IconGitHub from '@/assets/icons/IconGitHub.svg'
 
-const stylesIcon = `absolute fill-default-black h-[170px] left-[50%] -translate-x-[50%] top-[138px] w-[170px]
-dark:fill-default-lightGray`
+const stylesIcon = `absolute fill-default-black h-[120px] left-[50%] -translate-x-[50%] top-[30px] w-[170px]
+dark:fill-default-lightGray
+sm:h-[170px] sm:top-[54px]`
 
 export default function DefaultGitHubIcon(){
    return (
